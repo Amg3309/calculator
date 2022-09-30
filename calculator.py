e156366208d361d28c0df6eea0bc8d03ae406008
Amg3309 <69674073+Amg3309@users.noreply.github.com>
2022-09-30 19:01:03 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,9 +1,16 @@
-x = int(input("Enter First Number: "))
-y = int(input("Enter Second Number: "))
-print("Sum:", x + y)
-print("Difference:", x - y)
-print("Product:", x * y)
-print("Quotient:", x / y)
-print("Remainder:", x % y)
-print("Exponent:", x ** y)
-print("Floor Divided:", x // y)
+a=1
+
+def program():  
+  x = int(input("Enter First Number: "))
+  y = int(input("Enter Second Number: "))
+  print("Sum:", x + y)
+  print("Difference:", x - y)
+  print("Product:", x * y)
+  print("Quotient:", x / y)
+  print("Remainder:", x % y)
+  print("Exponent:", x ** y)
+  print("Floor Divided:", x // y)
+  
+  
+  while a==1:
+    program()
