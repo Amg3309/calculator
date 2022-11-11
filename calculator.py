2a419031f7ffb8677b1f2ce0e117cce93b24b18c
Amg3309 <69674073+Amg3309@users.noreply.github.com>
2022-11-11 11:28:40 +0530

    extra program

--- calculator.py
+++ calculator.py
@@ -14,3 +14,15 @@ def program():
   
   while a==1:
     program()
+
+"""def program():
+    a= int(input("Enter a number: "))
+    print("Table of ", a, " till 10")
+    for i in range(10):
+        i+=1
+        print(a*i)
+
+FakeVariable = 1
+
+while FakeVariable == 1:
+    program() """
